45d5ed7d53a19390825202f7e114cd6ffd5e775f
Nicolas Oyague <94309353+nico92-pe@users.noreply.github.com>
2023-05-01 09:10:38 -0500

    Add files via upload

---
+++ pset1/mario-more/mario.c
@@ -0,0 +1,51 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void pyramid(int n);
+
+int main(void)
+{
+    // Prompt number from user
+
+    int number;
+    do
+    {
+        number = get_int("Number of blocks: ");
+    }
+    while (number < 1 || number > 8);
+
+    // Make pyramid
+
+    pyramid(number);
+
+}
+
+void pyramid(int n)
+{
+    for (int j = 0; j < n; j++)
+    {
+
+        // Making the spaces
+        for (int i = 0; i < n - j - 1; i++)
+        {
+            printf(" ");
+        }
+
+        // Making the #
+        for (int l = 0; l < 2; l++)
+        {
+            for (int k = 0; k < j + 1; k++)
+            {
+                printf("#");
+            }
+
+            if (l == 0)
+            {
+                printf("  ");
+            }
+
+        }
+        printf("\n");
+
+    }
+}
